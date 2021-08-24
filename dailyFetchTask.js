075a5b543d3e4661b364ad524bdceb4a3053b987
amgarton47 <amgb2018@mymail.pomona.edu>
2021-08-23 19:22:02 -0500

    aa

--- dailyFetchTask.js
+++ dailyFetchTask.js
@@ -1,9 +1,8 @@
-const { fetchPuzzleData, db } = require("./index");
+const { fetchPuzzleData } = require("./index");
 
 const doTask = async () => {
   try {
     console.log("fetching today's data");
-    await db.sync();
     await fetchPuzzleData();
   } catch (err) {
     console.log(err);
