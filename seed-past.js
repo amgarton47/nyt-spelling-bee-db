203083b604cae333a57d5ca42e687911396babc3
amgarton47 <amgb2018@mymail.pomona.edu>
2022-06-18 01:46:23 -0500

    ?

--- seed-past.js
+++ seed-past.js
@@ -1,43 +1,43 @@
-// const { db, Puzzle } = require("./index");
-// const Sequelize = require("sequelize");
+const { db, Puzzle } = require("./index");
+const Sequelize = require("sequelize");
 
-// const localDB = new Sequelize(`postgres://localhost:5432/past-sb-data`, {
-//   logging: false,
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   //   ssl: process.env.DATABASE_URL,
-//   //   dialectOptions: {
-//   //     ssl: process.env.DATABASE_URL && {
-//   //       rejectUnauthorized: false,
-//   //       require: true,
-//   //     },
-//   //   },
-// });
+const localDB = new Sequelize(`postgres://localhost:5432/past-sb-data`, {
+  logging: false,
+  dialect: "postgres",
+  protocol: "postgres",
+  //   ssl: process.env.DATABASE_URL,
+  //   dialectOptions: {
+  //     ssl: process.env.DATABASE_URL && {
+  //       rejectUnauthorized: false,
+  //       require: true,
+  //     },
+  //   },
+});
 
-// const OldPuzzles = localDB.define("puzzle", {
-//   answers: Sequelize.ARRAY(Sequelize.STRING),
-//   displayDate: { type: Sequelize.STRING, primaryKey: true },
-//   validLetters: Sequelize.ARRAY(Sequelize.CHAR),
-//   centerLetter: Sequelize.CHAR,
-//   outerLetters: Sequelize.ARRAY(Sequelize.CHAR),
-//   pangrams: Sequelize.ARRAY(Sequelize.STRING),
-//   printDate: Sequelize.STRING,
-// });
+const OldPuzzles = localDB.define("puzzle", {
+  answers: Sequelize.ARRAY(Sequelize.STRING),
+  displayDate: { type: Sequelize.STRING, primaryKey: true },
+  validLetters: Sequelize.ARRAY(Sequelize.CHAR),
+  centerLetter: Sequelize.CHAR,
+  outerLetters: Sequelize.ARRAY(Sequelize.CHAR),
+  pangrams: Sequelize.ARRAY(Sequelize.STRING),
+  printDate: Sequelize.STRING,
+});
 
-// const init = async () => {
-//   try {
-//     // localDB.authenticate().then(() => console.log(localDB));
-//     const all = await OldPuzzles.findAll();
+const init = async () => {
+  try {
+    // localDB.authenticate().then(() => console.log(localDB));
+    const all = await OldPuzzles.findAll();
 
-//     all.forEach(async (p) => {
-//       Puzzle.create({ ...p.dataValues, nytId: null }).catch((err) =>
-//         console.log(err)
-//       );
-//       //   console.log(p.dataValues);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+    all.forEach(async (p) => {
+      Puzzle.create({ ...p.dataValues, nytId: null }).catch((err) =>
+        console.log(err)
+      );
+      //   console.log(p.dataValues);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// init();
+init();
